0dff33f2a2f1bf4cfc48f1feb8eebe8da8e751b7
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-01-09 02:57:37 +0700

    <feat>:[try dynamic route with github api]

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,25 @@
+"use client";
 import React from "react";
+import useSWR from "swr";
 
-function page() {
-  return <div>page</div>;
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function App({ params }: { params: { slug: string } }) {
+  const { data, error, isLoading } = useSWR(
+    `https://api.github.com/users/${params.slug}`,
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <img src={data.avatar_url} alt={data.name} />
+      {/* <p>{data.description}</p> */}
+      {/* <strong>👁 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong> */}
+    </div>
+  );
 }
-
-export default page;
